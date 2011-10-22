a4fe8421d924a34002b81e59d74588110eff073d
Brian Armstrong <barmstrong@airbnb.com>
2011-10-22 15:25:23 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ribbot::Application.config.secret_token = 'bd25e338971b62c93f0157f824ffe59ae02b8da4bb0c326de0fd7031480cd69f94fbafb3bbc0a88a95771e75b5726fb641fd3a4a85e14d89a2edbda13d0c6140'
