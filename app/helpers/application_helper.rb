622ef824ee0eba5a7681363baf9998b21cc3f8e2
Brian Armstrong <barmstrong@airbnb.com>
2011-11-25 16:24:33 -0800

    analytics code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,4 +39,21 @@ module ApplicationHelper
     controller.class.name.split("::").first
   end
   
+  def analytics_code
+    "<script type=\"text/javascript\">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-387546-19']);
+      _gaq.push(['_setDomainName', 'ribbot.com']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>".html_safe
+  end
+  
 end
