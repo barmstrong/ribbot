29603de59d82ffc5b280d711c9b710b558e90137
Brian Armstrong <barmstrong@airbnb.com>
2011-11-27 16:46:20 -0800

    verifications fix

--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -1,17 +1,17 @@
 class VerificationsController < ApplicationController
   #before_filter :authenticate_user!, :only => :index
-  
+
   def index
     if current_user.nil?
       redirect_to signin_path, :notice => "Please sign in first."
     end
   end
-  
+
   def create
     current_user.send_verification_email
     redirect_to verifications_path, :notice => "Verification email sent!"
   end
-  
+
   def show
     if params[:id].present? and user = User.where(:verification_token => params[:id]).first
       if !user.verified?
@@ -22,9 +22,9 @@ class VerificationsController < ApplicationController
       if current_user and p = current_user.participations.first
         path = root_url(:subdomain => p.forum.subdomain)
       end
-      redirect_to path, :notice => "Your account has been verified!"
+      redirect_to path, :notice => "Your account has been verified!"    
+    else
+      redirect_to verifications_path, :notice => "Couldn't find that verification code.  Please resend a new one."
     end
-  else
-    redirect_to verifications_path, :notice => "Couldn't find that verification code.  Please resend a new one."
   end
 end
