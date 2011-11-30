0ed5f71eebf6357a0c389611b0708724042c3f1a
Brian Armstrong <barmstrong@airbnb.com>
2011-11-29 22:43:59 -0800

    only render screenshots in production

--- app/helpers/account/themes_helper.rb
+++ app/helpers/account/themes_helper.rb
@@ -1,7 +1,11 @@
 module Account::ThemesHelper
   def theme_image theme, width=210
     height = (width.to_f * 0.8).round
-    url = u("http://support.ribbot.com/?theme_preview=#{theme.id}")
-    image_tag "http://api.thumbalizr.com/?url=#{url}&width=#{width}&api_key=b2b404b2204583aa14fb20c214003453", :size => "#{width}x#{height}"
+    if Rails.env.production?
+      url = u("http://support.ribbot.com/?theme_preview=#{theme.id}")
+      image_tag "http://api.thumbalizr.com/?url=#{url}&width=#{width}&api_key=b2b404b2204583aa14fb20c214003453", :size => "#{width}x#{height}"
+    else
+      image_tag "http://placehold.it/#{width}x#{height}", :size => "#{width}x#{height}"
+    end
   end
 end
