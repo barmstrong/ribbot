7fce1ded5136023ae155ac4fae07eb7f932da968
Brian Armstrong <barmstrong@airbnb.com>
2011-11-29 22:51:29 -0800

    theme helper

--- app/helpers/account/themes_helper.rb
+++ app/helpers/account/themes_helper.rb
@@ -2,7 +2,7 @@ module Account::ThemesHelper
   def theme_image theme, width=210
     height = (width.to_f * 0.8).round
     if Rails.env.production?
-      url = u("http://support.ribbot.com/?theme_preview=#{theme.id}")
+      url = "http://support.ribbot.com/?theme_preview=#{theme.id}"
       image_tag "http://api.thumbalizr.com/?url=#{url}&width=#{width}&api_key=b2b404b2204583aa14fb20c214003453", :size => "#{width}x#{height}"
     else
       image_tag "http://placehold.it/#{width}x#{height}", :size => "#{width}x#{height}"
