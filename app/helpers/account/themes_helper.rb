2f6a68e4d86fec38d3f9ec056aa6cb5e1a04ac9d
Brian Armstrong <barmstrong@airbnb.com>
2011-11-29 23:13:49 -0800

    thumblizr urls

--- app/helpers/account/themes_helper.rb
+++ app/helpers/account/themes_helper.rb
@@ -1,9 +1,9 @@
 module Account::ThemesHelper
   def theme_image theme, width=210
     height = (width.to_f * 0.8).round
-    if Rails.env.production?
+    if true or Rails.env.production?
       url = "http://support.ribbot.com/?theme_preview=#{theme.id}"
-      image_tag "http://api.thumbalizr.com/?url=#{url}&width=#{width}&api_key=b2b404b2204583aa14fb20c214003453", :size => "#{width}x#{height}"
+      image_tag "http://api.thumbalizr.com/?url=#{u url}&width=#{width}&api_key=b2b404b2204583aa14fb20c214003453", :size => "#{width}x#{height}"
     else
       image_tag "http://placehold.it/#{width}x#{height}", :size => "#{width}x#{height}"
     end
