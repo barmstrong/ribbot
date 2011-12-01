b46e00eb6c6f22e42bf2a3fe9f5325c14651cb19
Brian Armstrong <barmstrong@airbnb.com>
2011-11-30 22:31:35 -0800

    url2png

--- app/helpers/account/themes_helper.rb
+++ app/helpers/account/themes_helper.rb
@@ -1,9 +1,10 @@
 module Account::ThemesHelper
+  # these should go in an initializer but for some reason it can't read the variables when I do that
   Url2png::Config.public_key = 'P4ED717C89A831'
   Url2png::Config.shared_secret = 'SCFD8EF851C0B1'
   
   def theme_image theme, size="210x150"
-    if true or Rails.env.production?
+    if Rails.env.production?
       url = "http://support.ribbot.com/?theme_preview=#{theme.id}"
       site_image_tag(url, :size => size).html_safe
     else
---
+++ config/initializers/url2png.rb
@@ -0,0 +1,2 @@
+Url2png::Config.public_key = 'P4ED717C89A831'
+Url2png::Config.shared_secret = 'SCFD8EF851C0B1'
