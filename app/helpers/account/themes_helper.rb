1e5c7041e8ff67d4c43bbf98f7c7b2757218ee2b
Brian Armstrong <barmstrong@airbnb.com>
2011-12-05 20:07:39 -0800

    url2png rollback v3

--- app/helpers/account/themes_helper.rb
+++ app/helpers/account/themes_helper.rb
@@ -1,7 +1,11 @@
 module Account::ThemesHelper
   
   def theme_image theme, size="210x150"
-    url = "http://support.ribbot.com/?theme_preview=#{theme.id}"
-    site_image_tag(url, :size => size).html_safe
+    if Rails.env.production?
+      url = "http://support.ribbot.com/?theme_preview=#{theme.id}"
+      site_image_tag(url, :size => size).html_safe
+    else
+      "http://placehold.it/#{size}"
+    end
   end
 end
