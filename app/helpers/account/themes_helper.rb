ce851a723183cd1edfa87f5b27527a813be39d51
Brian Armstrong <barmstrong@airbnb.com>
2011-12-05 20:10:26 -0800

    url2png

--- app/helpers/account/themes_helper.rb
+++ app/helpers/account/themes_helper.rb
@@ -2,6 +2,8 @@ module Account::ThemesHelper
   
   def theme_image theme, size="210x150"
     if Rails.env.production?
+      Url2png::Config.public_key = 'P4ED717C89A831'
+      Url2png::Config.shared_secret = 'SCFD8EF851C0B1'
       url = "http://support.ribbot.com/?theme_preview=#{theme.id}"
       site_image_tag(url, :size => size).html_safe
     else
