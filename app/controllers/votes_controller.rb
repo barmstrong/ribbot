2c66215125db7ff0529193138bcec054991bdea1
Brian Armstrong <barmstrong@airbnb.com>
2012-03-12 13:34:07 -0700

    fix

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -15,7 +15,7 @@ class VotesController < ApplicationController
       @error = "You are voting too fast.  Please wait a bit."
     end
     
-    if current_user.created_at > 1.month.ago
+    if current_user.created_at > 1.week.ago and params[:direction].to_sym == :down
       allowed = false
       @error = "You need more reputation before you downvote."
     end
