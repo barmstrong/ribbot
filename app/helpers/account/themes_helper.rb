4bb6a6f43519a286abf9fe4bfe2339878c0e871c
Brian Armstrong <barmstrong@gmail.com>
2012-11-12 15:50:08 -0800

    same

--- app/helpers/account/themes_helper.rb
+++ app/helpers/account/themes_helper.rb
@@ -12,6 +12,6 @@ module Account::ThemesHelper
   def webyshots_url(url, size)
     safe_url = CGI.escape(url)
     token = Digest::MD5.hexdigest("#{ENV['WEBYSHOTS_SECRET']}+#{safe_url}")
-    "http://api.webyshots.com/v1/shot/#{ENV['WEBYSHOT_KEY']}/#{token}/?url=#{CGI.escape url}&dimension=#{size}&format=png&cache=false"
+    "http://api.webyshots.com/v1/shot/#{ENV['WEBYSHOT_KEY']}/#{token}/?url=#{CGI.escape url}&dimension=#{size}&format=png"
   end
 end
