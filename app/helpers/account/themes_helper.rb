fddfe2e9dc0f74bbfc8803b55113a7865cb145e1
Brian Armstrong <barmstrong@gmail.com>
2012-11-12 20:31:54 -0800

    webyshots

--- app/helpers/account/themes_helper.rb
+++ app/helpers/account/themes_helper.rb
@@ -2,7 +2,7 @@ module Account::ThemesHelper
   
   def theme_image theme, size="210x150"
     if Rails.env.production?
-      url = "support.ribbot.com/?theme_preview=#{theme.id}"
+      url = "http://support.ribbot.com/?theme_preview=#{theme.id}"
       image_tag(webyshots_url(url, size), :size => size)
     else
       image_tag "http://placehold.it/#{size}"
@@ -11,7 +11,7 @@ module Account::ThemesHelper
 
   def webyshots_url(url, size)
     safe_url = CGI.escape(url)
-    token = Digest::MD5.hexdigest("#{ENV['WEBYSHOTS_SECRET']}+#{safe_url}")
+    token = Digest::MD5.hexdigest("#{ENV['WEBYSHOTS_SECRET']}+#{url}")
     "http://api.webyshots.com/v1/shot/#{ENV['WEBYSHOTS_KEY']}/#{token}/?url=#{safe_url}&dimension=#{size}&format=png".html_safe
   end
 end
